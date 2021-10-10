9cce1fd5b56a37f5c80501cd4f4ccfab2b1a3fd6
Adrian Borucki <ab@synthillect.ai>
2021-10-10 17:43:17 +0200

    Add an example with ImageBlur and ImageWrite

---
+++ examples/blur_and_write.scm
@@ -0,0 +1,18 @@
+; An example of loading an image, applying a blur filter on it and storing the result in a file
+(use-modules (opencog) (opencog exec) (opencog vision))
+
+; Load an example image.
+(define img-node (Image "example_image.png"))
+
+; Define a node where we can attach values.
+(define img-collection (Concept "Image Collection"))
+(define key-1 (Concept "image#1"))
+
+; Setup an ImageBlurLink and store the result of its execution under `img-collection key-1`.
+(define img-blur (ImageBlur img-node (Number "5")))
+(cog-set-value! img-collection key-1 (cog-execute! img-blur))
+
+; Setup an ImageWriteLink with the image to be loaded from `img-collection key-1` and
+; stored in `example_output.png`.
+(define img-write (ImageWrite (ValueOf img-collection key-1) (Concept "example_output.png")))
+(cog-execute! img-write)
