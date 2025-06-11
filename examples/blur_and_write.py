8de149d8d30887a25becd88b4f4d06a3e91cb20f
Linas Vepštas <linasvepstas@gmail.com>
2025-06-10 19:47:22 -0500

    iMinor indentation cleanup

--- examples/blur_and_write.py
+++ examples/blur_and_write.py
@@ -14,19 +14,20 @@ from opencog.vision import *
 set_default_atomspace(AtomSpace())
 
 # Load an example image
-img_node = ImageNode("example_image.png")
+img_node = Image("example_image.png")
 
 # Define a node where we can attach values
-img_collection = ConceptNode("Image Collection")
-key_1 = ConceptNode("image#1")
+img_collection = Concept("Image Collection")
+key_1 = Concept("image#1")
 
-# Setup an ImageBlurLink and store the result of its execution under img_collection key_1
-img_blur = ImageBlurLink(img_node, NumberNode("5"))
+# Setup an ImageBlurLink and store the result of its execution
+# under img_collection key_1
+img_blur = ImageBlur(img_node, NumberNode("10"))
 result = execute_atom(atomspace, img_blur)
 img_collection.set_value(key_1, result)
 
-# Setup an ImageWriteLink with the image to be loaded from img_collection key_1 and
-# stored in example_output.png
+# Setup an ImageWriteLink with the image to be loaded from
+# img_collection key_1 and stored in example_output.png
 img_write = ImageWriteLink(ValueOfLink(img_collection, key_1), ConceptNode("example_output.png"))
 execute_atom(atomspace, img_write)
 
