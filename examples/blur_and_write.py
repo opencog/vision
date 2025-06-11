6ea610cda73aad8facc1920e1697fbfb8efe2e9d
Linas Vepštas <linasvepstas@gmail.com>
2025-06-10 20:44:07 -0500

    Bugfix the example

--- examples/blur_and_write.py
+++ examples/blur_and_write.py
@@ -11,7 +11,8 @@ from opencog.type_constructors import *
 from opencog.vision import *
 
 # Create an AtomSpace and set it as default
-set_default_atomspace(AtomSpace())
+asp = AtomSpace()
+set_default_atomspace(asp)
 
 # Load an example image
 img_node = Image("example_image.png")
@@ -23,12 +24,12 @@ key_1 = Concept("image#1")
 # Setup an ImageBlurLink and store the result of its execution
 # under img_collection key_1
 img_blur = ImageBlur(img_node, NumberNode("10"))
-result = execute_atom(atomspace, img_blur)
+result = img_blur.execute()
 img_collection.set_value(key_1, result)
 
 # Setup an ImageWriteLink with the image to be loaded from
 # img_collection key_1 and stored in example_output.png
 img_write = ImageWriteLink(ValueOfLink(img_collection, key_1), ConceptNode("example_output.png"))
-execute_atom(atomspace, img_write)
+img_write.execute()
 
 print("Image processed successfully. Output saved to example_output.png")
